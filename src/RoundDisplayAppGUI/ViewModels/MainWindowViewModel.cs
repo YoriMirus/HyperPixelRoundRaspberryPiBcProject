eab3618ea84fce6ad871efb9b959eed7724cab33
yorimirus <misawalach00@seznam.cz>
2025-09-28 21:11:33 +0200

    Přidat widget pro hodiny (1/2) Zatím mám jenom sekundovou ručičku, zbytek musím ještě nadesignovat

--- src/RoundDisplayAppGUI/ViewModels/MainWindowViewModel.cs
+++ src/RoundDisplayAppGUI/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,56 @@
-﻿namespace RoundDisplayAppGUI.ViewModels;
+﻿using System;
+
+namespace RoundDisplayAppGUI.ViewModels;
+
+using Avalonia.Media;
+using Avalonia;
+using CommunityToolkit.Mvvm.ComponentModel;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
     public string Greeting { get; } = "Welcome to Avalonia!";
+
+    public ITransform RotationTransform
+    {
+        get => _rotationTransform;
+        set
+        {
+            _rotationTransform = value;
+            this.OnPropertyChanged(nameof(RotationTransform));
+        }
+    }
+    private ITransform _rotationTransform;
+
+    public MainWindowViewModel()
+    {
+        RotationTransform = new RotateTransform()
+        {
+            Angle = 0, CenterX = 480 / 2,
+            CenterY = 480 / 2
+        };
+    }
+    
+    public void SetSecondHand(byte seconds)
+    {
+        if (seconds > 60)
+        {
+            throw new ArgumentOutOfRangeException(nameof(seconds), seconds, "Seconds must have a value from 0 to 60.");
+        }
+        
+        // 0 sekund je -90 stupňů.
+        // 15 sekund je 0 stupňů
+        // 30 sekund je 90 stupňů
+        // 45 sekund je 180 stupňů
+        // 60 sekund je -90 (270) stupňů
+
+        int angle = (90/15) * seconds - 90;
+
+        RotationTransform = new RotateTransform()
+        {
+            Angle = angle,
+            CenterX = 0.5,
+            CenterY = 0.5
+        };
+        
+    }
 }
--- src/RoundDisplayAppGUI/Views/MainWindow.axaml.cs
+++ src/RoundDisplayAppGUI/Views/MainWindow.axaml.cs
@@ -1,4 +1,8 @@
+using System;
 using Avalonia.Controls;
+using Avalonia.Media;
+using Avalonia.Threading;
+using RoundDisplayAppGUI.ViewModels;
 
 namespace RoundDisplayAppGUI.Views;
 
@@ -7,5 +11,27 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        //DataContext = new MainWindowViewModel();
+
+        DispatcherTimer.Run(OnTimerTick, TimeSpan.FromMilliseconds(100), DispatcherPriority.ApplicationIdle);
+        
+        this.SecondHandImage.RenderTransform = new RotateTransform()
+        {
+            Angle = 69,
+            CenterX = 0.5,
+            CenterY = 0.5
+        };
+    }
+
+    bool OnTimerTick()
+    {
+        if (DataContext is null)
+            return true;
+        
+        ((MainWindowViewModel)DataContext).SetSecondHand((byte)DateTime.Now.Second);
+        
+        // Časovač potřebuje vědět, zda má tuto metodu zavolat znova
+        // Nemáme důvod to přerušovat, takže vždycky dáváme true, a.k.a. pokračovat časování
+        return true;
     }
 }
