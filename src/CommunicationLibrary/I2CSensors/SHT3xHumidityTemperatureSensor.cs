67993ff4c6076c3d0cb3c6f362c2c4a21da5ffad
yorimirus <misawalach00@seznam.cz>
2025-11-09 19:10:47 +0100

    Přidej senzor SHT30 Tento senzor čte teplotu a vlhkost

---
+++ src/CommunicationLibrary/I2CSensors/SHT3xHumidityTemperatureSensor.cs
@@ -0,0 +1,60 @@
+namespace CommunicationLibrary.I2CSensors;
+
+using Iot.Device.Sht3x;
+using System.Device.I2c;
+
+public class SHT3xHumidityTemperatureSensor : ISensorDataSource<Tuple<double,double>>
+{
+    private Sht3x? SHT3xDevice { get; set; }
+    private I2cDevice? I2CDevice { get; set; }
+    private int I2CBusNumber { get; }
+    private int DurationBetweenReads { get; }
+    
+    public void Dispose()
+    {
+        SHT3xDevice?.Dispose();
+        I2CDevice?.Dispose();
+        SHT3xDevice = null;
+        I2CDevice = null;
+    }
+    
+    public event SensorDataReceivedHandler<Tuple<double, double>>? OnDataReceived;
+    public void StartListening()
+    {
+        if (SHT3xDevice is not null && I2CDevice is not null)
+        {
+            SHT3xDevice.Dispose();
+            I2CDevice.Dispose();
+        }
+
+        var i2CSettings = new I2cConnectionSettings(I2CBusNumber, 0x45);
+        I2CDevice = I2cDevice.Create(i2CSettings);
+        SHT3xDevice = new Sht3x(I2CDevice);
+
+        Task.Run(ReadingLoop);
+    }
+
+    public void StopListening()
+    {
+        Dispose();
+    }
+    
+    private async Task ReadingLoop()
+    {
+        while (SHT3xDevice is not null && I2CDevice is not null)
+        {
+            var temp = SHT3xDevice.Temperature.DegreesCelsius;
+            var humidity = SHT3xDevice.Humidity.Percent;
+            
+            OnDataReceived?.Invoke(this, new SensorDataEventArgs<Tuple<double, double>>(DateTime.Now, new Tuple<double,double>(temp, humidity)));
+            
+            await Task.Delay(DurationBetweenReads);
+        }
+    }
+
+    public SHT3xHumidityTemperatureSensor(int i2CBusNumber, int durationBetweenReads)
+    {
+        I2CBusNumber = i2CBusNumber;
+        DurationBetweenReads = durationBetweenReads;
+    }
+}
---
+++ src/SensorTesting/Program.cs
@@ -0,0 +1,66 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using CommunicationLibrary.I2CSensors;
+using CommunicationLibrary;
+
+Console.WriteLine("Tester senzorů. Zadej číslo i2c sběrnice (podívej se do /dev a zkus svůj senzor najít pomocí i2cdetect)");
+string? input = "...";
+int i2CBusNumber;
+while (!int.TryParse(input, out i2CBusNumber))
+{
+    input = Console.ReadLine();
+}
+
+Console.WriteLine("Který senzor chceš použít?");
+Console.WriteLine("0 = BMP180 GY-68 (nadmořská výška, atmosférický tlak, teplota)");
+Console.WriteLine("1 = SHT30 (teplota, vlhkost)");
+
+ISensorDataSource<Tuple<double,double,double>>? BmpSensor = null;
+ISensorDataSource<Tuple<double, double>>? Sht30Sensor = null;
+
+bool cancel = false;
+while (!cancel)
+{
+    input = Console.ReadLine();
+    switch (input)
+    {
+        case "0":
+            BmpSensor = new Bmp180Gy68PressureTemperatureSensor(i2CBusNumber, 1000);
+            BmpSensor.OnDataReceived += DataReceived;
+            BmpSensor.StartListening();
+            cancel = true;
+            break;
+        case "1":
+            Sht30Sensor = new SHT3xHumidityTemperatureSensor(i2CBusNumber, 1000);
+            Sht30Sensor.OnDataReceived += Sht30SensorOnOnDataReceived;
+            Sht30Sensor.StartListening();
+            cancel = true;
+            break;
+    }
+}
+
+while (!Console.KeyAvailable)
+{
+    
+}
+
+if (BmpSensor is not null)
+    BmpSensor.Dispose();
+
+if (Sht30Sensor is not null)
+    Sht30Sensor.Dispose();
+
+return;
+
+void DataReceived(object sender, SensorDataEventArgs<Tuple<double, double, double>> e)
+{
+    Console.WriteLine("Nadmořská výška (m): " + e.Value.Item1);
+    Console.WriteLine("Atmosférický tlak (atm): " + e.Value.Item2);
+    Console.WriteLine("Teplota (°C): " + e.Value.Item3);
+}
+
+void Sht30SensorOnOnDataReceived(object sender, SensorDataEventArgs<Tuple<double, double>> e)
+{
+    Console.WriteLine("Teplota (°C): " + e.Value.Item1);
+    Console.WriteLine("Vlhkost (%): " + e.Value.Item2);
+}
