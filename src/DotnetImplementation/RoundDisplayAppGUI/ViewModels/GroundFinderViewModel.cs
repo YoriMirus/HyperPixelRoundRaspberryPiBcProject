2153616579f4718221f4b783de0d12aa622d02a1
yorimirus <misawalach00@seznam.cz>
2025-11-20 21:22:58 +0100

    Přesuň c# projekt do své vlastní složky v src

--- src/DotnetImplementation/RoundDisplayAppGUI/ViewModels/GroundFinderViewModel.cs
+++ src/DotnetImplementation/RoundDisplayAppGUI/ViewModels/GroundFinderViewModel.cs
@@ -42,7 +42,6 @@ public class GroundFinderViewModel : ViewModelBase
     
     private void SensorOnOnDataReceived(object sender, SensorDataEventArgs<AccelerometerDTO> e)
     {
-        /* OLD CODE HERE
         double y = e.Value.AccelerationY;
         double x = e.Value.AccelerationX;
         
@@ -52,39 +51,6 @@ public class GroundFinderViewModel : ViewModelBase
         Dispatcher.UIThread.Invoke(() =>
         {
             RotateTransform = new RotateTransform(angleDeg);
-        });*/
-
-        double x = e.Value.AccelerationX;
-        double y = e.Value.AccelerationY;
-        double z = e.Value.AccelerationZ;
-        
-        // 1. Normalize the accelerometer vector
-        double len = Math.Sqrt(x * x + y * y + z * z);
-        if (len < 1e-6) return; // avoid division by zero
-        double gx = x / len;
-        double gy = y / len;
-        double gz = z / len;
-        
-        // Tilt forward/back (X rotation)
-        double angleXRad = Math.Atan2(-gz, gy); // negative gz because screen Z points out
-        double angleXDeg = angleXRad * 180.0 / Math.PI;
-
-        // Tilt left/right (Y rotation)
-        double angleYRad = Math.Atan2(-gz, gx); // negative gz for correct tilt
-        double angleYDeg = angleYRad * 180.0 / Math.PI;
-
-        // Z rotation not needed for flat arrow
-        double angleZDeg = 0;
-
-        // 3. Apply rotation on the UI thread
-        Dispatcher.UIThread.Invoke(() =>
-        {
-            RotateTransform = new Rotate3DTransform()
-            {
-                AngleX = angleXDeg,
-                AngleY = angleYDeg,
-                AngleZ = angleZDeg
-            };
         });
     }
 }
