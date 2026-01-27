a50ed622ddcac15b604e3e66b6cc43f1bbc5b05e
yorimirus <misawalach00@seznam.cz>
2026-01-27 16:32:28 +0100

    Fix error při SIGINT + možné neukončení programu

--- src/PythonImplementation/main.py
+++ src/PythonImplementation/main.py
@@ -5,7 +5,7 @@ import socket
 from datetime import datetime
 from PySide6.QtWidgets import QApplication, QWidget
 from PySide6.QtGui import QPixmap, QPainter, QTransform, QFontDatabase
-from PySide6.QtCore import Qt, QTimer
+from PySide6.QtCore import Qt, QTimer, QCoreApplication
 
 from widgets.MainWindow import MainWindow
 
@@ -16,8 +16,7 @@ def is_pi_environment():
 
 def handle_sigint(sig, frame):
     print("Caught SIGINT, quitting...")
-    QApplication.quit()
-    sys.exit(0)
+    QCoreApplication.exit(0)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
